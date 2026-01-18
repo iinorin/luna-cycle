57d5c0353a0de2ef1d85baedb2fb7d6f113bcb83
Kaushik Tiwari <raiderkaushik11@gmail.com>
2026-01-19 00:17:39 +0530

    update energyBar.tsx and EnergySelector.tsx

--- app/(drawer)/energyBar.tsx
+++ app/(drawer)/energyBar.tsx
@@ -3,16 +3,8 @@ import EnergySelector from "@/src/energy/EnergySelector";
 
 export default function EnergyBarScreen() {
   return (
-    <View style={styles.container}>
-      <EnergySelector />
-    </View>
+
+    <EnergySelector />
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "#fff",
-  },
-});
