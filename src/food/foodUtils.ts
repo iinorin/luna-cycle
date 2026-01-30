15c5713bc35ed05d9293f609c600bbd62b4717f8
Kaushik Tiwari <raiderkaushik11@gmail.com>
2026-01-30 22:27:16 +0530

    chore: delete foodUtils.ts

--- src/food/foodUtils.ts
+++
@@ -1,7 +0,0 @@
-import { foodSuggestions, CyclePhase } from "./foodTypes";
-
-export const getFoodsForPhase = (phase: CyclePhase) => {
-  return foodSuggestions.filter((food) => 
-    food.recommendedPhase.includes(phase)
-  );
-};
